48ba04719f4362d0e340dafa7f5f40474a077c41
21stChrom <parkerjmartin2020@gmail.com>
2022-08-17 20:00:20 -0400

    1 file modified

--- src/App.js
+++ src/App.js
@@ -4,9 +4,18 @@ import "./style.css";
 export default function App() {
   return (
     <div class="container">
+      <head>
+        <title>Parker's App</title>
+      </head>
       <div>
       <header>AppIcon Time Counter</header>
-      <p></p>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
       </div>
     </div>
   );
